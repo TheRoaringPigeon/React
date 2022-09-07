fce854890d0daec62136b3bf12d45b9a67b3f552
TheRoaringPigeon <107893565+TheRoaringPigeon@users.noreply.github.com>
2022-09-07 11:27:38 -0400

    1) Cleanup of previous code. 2) Chart feature added.

--- react-complete-guide/src/components/ExpensesComponents/Expenses.js
+++ react-complete-guide/src/components/ExpensesComponents/Expenses.js
@@ -1,38 +1,28 @@
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
 function Expenses(props) {
-  const [filterYear, setFilterYear] = useState('2020');
-  const filterChangeHandler = selectedYear => {
+  const [filterYear, setFilterYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear() === parseInt(filterYear);
+  });
+  
   return (
-      <Card className="expenses">
-      <ExpensesFilter selected={filterYear} onFilterChange={filterChangeHandler}/>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filterYear}
+        onFilterChange={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses}/>
     </Card>
-    
   );
 }
 
